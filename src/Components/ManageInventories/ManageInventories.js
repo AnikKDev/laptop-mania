c443d3499414e3565a15bad464fc66ef5fc924cb
AnikKDev <anikdev2016@gmail.com>
2022-04-30 20:21:07 +0600

    completed delete item function

--- src/Components/ManageInventories/ManageInventories.js
+++ src/Components/ManageInventories/ManageInventories.js
@@ -15,6 +15,25 @@ const ManageInventories = () => {
         };
         getAllItems();
     }, []);
+
+
+    // handle remove item by clicking on delte button
+    const handleRemoveItem = (id) => {
+        const proceed = window.confirm('Are you sure you want to delete?')
+        if (proceed) {
+            console.log(id);
+            const url = `http://localhost:5000/inventory/${id}`;
+            fetch(url, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data, 'successfully deleted');
+                    const remaining = allItems.filter(item => item._id !== id);
+                    setAllItems(remaining);
+                })
+        }
+    };
     return (
         <div>
             Manage Inventories:{allItems.length}
@@ -26,20 +45,22 @@ const ManageInventories = () => {
                             <th>Item Name</th>
                             <th>Supplier Name</th>
                             <th>Quantity</th>
-                            <th>Price</th>
+                            <th>Price (Per Item)</th>
                             <th>Category</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             allItems.map(item => {
-                                return <tr>
+
+                                return <tr key={item._id}>
 
                                     <td>{item?.itemName}</td>
                                     <td>{item?.supplierName}</td>
                                     <td>{item?.quantity}</td>
                                     <td>{item?.price}</td>
                                     <td>{item?.category}</td>
+                                    <td><button onClick={() => handleRemoveItem(item._id)} className="btn btn-danger">Remove</button></td>
                                 </tr>
                             })
                         }
