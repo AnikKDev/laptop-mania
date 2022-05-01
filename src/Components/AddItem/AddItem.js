0ae40378871018f2ff8cac5e819f0d37167e47ae
AnikKDev <anikdev2016@gmail.com>
2022-05-01 12:35:10 +0600

    completed add item input section

--- src/Components/AddItem/AddItem.js
+++ src/Components/AddItem/AddItem.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
 import { useForm } from "react-hook-form";
+import axios from 'axios';
 const AddItem = () => {
 
     // handle add item button
     const { register, handleSubmit } = useForm();
     const onSubmit = data => {
-        console.log(data)
+        console.log(data);
+        axios.post('http://localhost:5000/manage-inventories', data)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     };
     return (
         <div>
@@ -18,6 +26,7 @@ const AddItem = () => {
                     <input className="my-3" placeholder='Item Name' {...register("itemName", { required: true, maxLength: 20 })} />
                     <input className="my-3" placeholder='Supplier Name' {...register("supplierName", { required: true })} />
                     <input className="my-3" placeholder='Price Per Item' type="number" {...register("price", { required: true })} />
+                    <input className="my-3" placeholder='Category' type="text" {...register("category", { required: true })} />
                     <textarea className="my-3" placeholder='Item Description' type="text" {...register("description", { required: true })} />
                     <input className="my-3" placeholder='Image URL' {...register("img", { required: true })} />
                     <input className="my-3" placeholder='Quantity (min: 5)' type="number" {...register("quantity", { min: 5, required: true })} />
