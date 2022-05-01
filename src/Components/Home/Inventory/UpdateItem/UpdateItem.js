6013bca4c1f545732bd1ca18f3d794e6c3d2069f
AnikKDev <anikdev2016@gmail.com>
2022-05-01 21:35:45 +0600

    completed increasing and decreaasing quantity

--- src/Components/Home/Inventory/UpdateItem/UpdateItem.js
+++ src/Components/Home/Inventory/UpdateItem/UpdateItem.js
@@ -7,36 +7,58 @@ const UpdateItem = () => {
     const { id } = useParams();
     // console.log(id);
     const [singleItem, setSingleItem] = useState({});
+    const [restock, setRestock] = useState('');
+
+
     const { img, description, supplierName, itemName, quantity, price } = singleItem;
-    // const [itemQuantity, setItemQuantity] = useState(0);
-    // useEffect(() => {
-    //     setItemQuantity(quantity - 1)
-    // }, [quantity]);
 
     // handle update button
-    const handleQuantityUpdate = (id) => {
-        const newQuantity = parseInt(quantity - 1);
-        if (newQuantity < 0) {
-            return
+    const handleQuantityUpdate = (id, isTrue) => {
+        let newQuantity;
+        if (isTrue) {
+            console.log('updated quantity (Decreased)');
+            newQuantity = parseInt(quantity - 1);
+            if (newQuantity < 0) {
+                return
+            }
+            fetch(`http://localhost:5000/inventory/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({ newQuantity: newQuantity })
+            })
+                .then(response => response.json())
+                .then(result => {
+                    console.log('Success:', result);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+            console.log(id);
+        } else {
+            console.log('updated quantity (increased)');
+            newQuantity = parseInt(parseInt(quantity) + parseInt(restock));
+            fetch(`http://localhost:5000/inventory/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({ newQuantity: newQuantity })
+            })
+                .then(response => response.json())
+                .then(result => {
+                    console.log('Success:', result);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+            console.log(id);
         }
 
-        fetch(`http://localhost:5000/inventory/${id}`, {
-            method: 'PUT',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify({ newQuantity: newQuantity })
-        })
-            .then(response => response.json())
-            .then(result => {
-                console.log('Success:', result);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-        console.log(id);
-
     };
+
+
     useEffect(() => {
         async function getSingleItem() {
             try {
@@ -48,6 +70,7 @@ const UpdateItem = () => {
         };
         getSingleItem();
     });
+
 
 
 
@@ -67,13 +90,13 @@ const UpdateItem = () => {
                             <h5>Single Price: {price}</h5>
                             <h5>Quantity: {quantity}</h5>
                             <p className="text-muted">{description}</p>
-                            {quantity === 0 ? <button disabled className="btn btn-warning my-2">Sold</button> : <button onClick={() => handleQuantityUpdate(id)} className="btn btn-warning px-3">Delivered</button>}
+                            {quantity === 0 ? <button disabled className="btn btn-warning my-2">Sold</button> : <button onClick={() => handleQuantityUpdate(id, true)} className="btn btn-warning px-3">Delivered</button>}
 
                         </div>
-                        <form className="w-25 mt-4">
-                            <Form.Control type="number" placeholder="Restock" />
-                            <button className="btn btn-warning my-2">Restock Item</button>
-                        </form>
+                        <div className="w-25 mt-4" >
+                            <Form.Control onChange={event => setRestock(event?.target?.value)} type="number" placeholder="Restock" />
+                            <button onClick={() => handleQuantityUpdate(id, false)} className="btn btn-warning my-2">Restock Item</button>
+                        </div>
                         <Link to="/manage-inventories">
                             <button className="btn btn-warning">Manage Inventory</button>
                         </Link>
