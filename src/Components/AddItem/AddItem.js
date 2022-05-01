f0e41b4624742040a63bcb76937d1b5e138dff82
AnikKDev <anikdev2016@gmail.com>
2022-05-01 12:12:42 +0600

    added input field from react form hook

--- src/Components/AddItem/AddItem.js
+++ src/Components/AddItem/AddItem.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
-
+import { useForm } from "react-hook-form";
 const AddItem = () => {
 
     // handle add item button
-
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+    };
     return (
         <div>
             <h2>
                 add items
             </h2>
             <div className="container">
-
+                <form className="w-50 mx-auto d-flex flex-column" onSubmit={handleSubmit(onSubmit)}>
+                    <input className="my-3" placeholder='Item Name' {...register("itemName", { required: true, maxLength: 20 })} />
+                    <input className="my-3" placeholder='Supplier Name' {...register("supplierName", { required: true })} />
+                    <input className="my-3" placeholder='Price Per Item' type="number" {...register("price", { required: true })} />
+                    <textarea className="my-3" placeholder='Item Description' type="text" {...register("description", { required: true })} />
+                    <input className="my-3" placeholder='Image URL' {...register("img", { required: true })} />
+                    <input className="my-3" placeholder='Quantity (min: 5)' type="number" {...register("quantity", { min: 5, required: true })} />
+                    <input className="my-3" type="submit" />
+                </form>
             </div>
         </div>
     );
