fa97e2cf554845fa68351da778273e5aee2ac060
AnikKDev <anikdev2016@gmail.com>
2022-05-04 15:17:27 +0600

    completed header styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -14,14 +14,14 @@ const Header = () => {
     return (
         <Navbar bg="" expand="lg" className="navigation-container">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand className="nav-title" href="#home">Laptop <span className="text-danger">Mania</span></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/about">About</Nav.Link>
-                        <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
-                        <Nav.Link as={Link} to="/my-items">My Items</Nav.Link>
+                    <Nav className="ms-auto nav-links">
+                        <Nav.Link className="mx-2" as={Link} to="/home">Home</Nav.Link>
+                        <Nav.Link className="mx-2" as={Link} to="/about">About</Nav.Link>
+                        <Nav.Link className="mx-2" as={Link} to="/blogs">Blogs</Nav.Link>
+                        <Nav.Link className="mx-2" as={Link} to="/my-items">My Items</Nav.Link>
                         {user ? <button onClick={logout} className="btn btn-danger">Logout</button> : <Nav.Link as={Link} to="/login">Login</Nav.Link>}
                     </Nav>
                 </Navbar.Collapse>
